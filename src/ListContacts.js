e7fcad3c533e4dbb143e75a11c7a8f7532132743
Vikrant Sharma <vikkrant.xx7@gmail.com>
2018-11-21 23:01:14 +0530

    Initial Commit

---
+++ src/ListContacts.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class ListContacts extends React.Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+    state = {
+        query: ''
+    }
+    updateQuery = (query) => {
+        this.setState({query: query.trim()})
+    }
+    render() {
+        return (
+            <div className='list-contacts'>
+            {JSON.stringify(this.state)}
+                <div className='list-contacts-top'>
+                    <input className='search-contacts' type='text' placeholder='Search Contacts' value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)}/>
+                </div>
+                <ol className='contact-list'>
+                    {this.props.contacts.map((contact) => {
+                        return (<li key={contact.id} className='contact-list-item'>
+                            <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}/>
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>Remove</button>
+                        </li>);
+                    })}
+                </ol>
+            </div>
+        )
+    }
+}
+
+export default ListContacts
